167d0137a4caa37d15c887981f33fce918eea8f1
Jay Chan <34962937+chan111222333@users.noreply.github.com>
2021-04-10 23:44:55 +0800

    update

--- src/env/env.py
+++ src/env/env.py
@@ -5,6 +5,7 @@ class BitcoinTradeEnv():
     def __init__(self, trading_data_csv_path, env_config):
         self.trading_records = read_bitcoin_history(trading_data_csv_path)
         self.trading_open_price = self.trading_records[:, 0]
+        self.trading_close_price = self.trading_records[:, -3]
         self.env_config = env_config
 
     def _prep_obs(self):
@@ -12,22 +13,68 @@ class BitcoinTradeEnv():
         the obs is not yet normalized.
         :return:
         '''
-        raw_rep = self.trading_records[self.trading_index, :]
-        rep = np.concatenate([raw_rep, self.current_cash_value, self.current_asset_value], axis=0)
-        return rep
+        if self.act_count == 0:
+            raw_rep = self.trading_records[self.trading_index-3:self.trading_index+1, :]
+            extra_rep = np.array([1, 0, 1, 0, 1, 0, 1, 0])*self.env_config.initial_cash_value
+            obs = np.concatenate([raw_rep, extra_rep.reshape(-1, 2)], axis=1)
+        elif self.act_count == 1:
+            raw_rep = self.trading_records[self.trading_index-3:self.trading_index+1, :]
+            extra_rep = np.concatenate(
+                [
+                    np.array([1, 0, 1, 0, 1, 0])*self.env_config.initial_cash_value,
+                    np.array([self.current_cash_value, self.current_asset_unit*raw_rep[-1, -3]])
+                ],
+                axis=0
+            )
+            obs = np.concatenate([raw_rep, extra_rep.reshape(-1, 2)], axis=1)
+        elif self.act_count == 2:
+            raw_rep = self.trading_records[self.trading_index-3:self.trading_index+1, :]
+            extra_rep = np.concatenate(
+                [
+                    np.array([1, 0, 1, 0])*self.current_cash_value,
+                    np.array([self.asset_cash_history[-2][0], self.asset_cash_history[-2][1]*raw_rep[-2, -3]]),
+                    np.array([self.asset_cash_history[-1][0], self.asset_cash_history[-1][1]*raw_rep[-1, -3]])
+                ],
+                axis=0
+            )
+            obs = np.concatenate([raw_rep, extra_rep.reshape(-1, 2)], axis=1)
+        elif self.act_count == 3:
+            raw_rep = self.trading_records[self.trading_index-3:self.trading_index+1, :]
+            extra_rep = np.concatenate(
+                [
+                    np.array([1, 0])*self.current_cash_value,
+                    np.array([self.asset_cash_history[-3][0], self.asset_cash_history[-3][1]*raw_rep[-3, -3]]),
+                    np.array([self.asset_cash_history[-2][0], self.asset_cash_history[-2][1]*raw_rep[-2, -3]]),
+                    np.array([self.asset_cash_history[-1][0], self.asset_cash_history[-1][1]*raw_rep[-1, -3]])
+                ],
+                axis=0
+            )
+            obs = np.concatenate([raw_rep, extra_rep.reshape(-1, 2)], axis=1)
+        else:
+            raw_rep = self.trading_records[self.trading_index-3:self.trading_index+1, :]
+            extra_rep = np.concatenate(
+                [
+                    np.array([self.asset_cash_history[-4][0], self.asset_cash_history[-4][1]*raw_rep[-4, -3]]),
+                    np.array([self.asset_cash_history[-3][0], self.asset_cash_history[-3][1]*raw_rep[-3, -3]]),
+                    np.array([self.asset_cash_history[-2][0], self.asset_cash_history[-2][1]*raw_rep[-2, -3]]),
+                    np.array([self.asset_cash_history[-1][0], self.asset_cash_history[-1][1]*raw_rep[-1, -3]])
+                ],
+                axis=0
+            )
+            obs = np.concatenate([raw_rep, extra_rep.reshape(-1, 2)], axis=1)
+
+        return obs
 
     def reset(self):
         self.act_count = 0
-        self.total_capital_history = [self.env_config.initial_cash_value]
         self.current_cash_value = self.env_config.initial_cash_value
         self.current_asset_unit = 0
-        self.trading_index = np.random.randint(0, self.trading_records.shape[0] - self.env_config.episode_length)
+        self.asset_cash_history = [[self.current_cash_value, self.current_asset_unit]]
+        self.total_capital_history = [self.current_cash_value]
+        self.trading_index = np.random.randint(3, self.trading_records.shape[0] - self.env_config.episode_length)
         self.obs = self._prep_obs()
 
         return self.obs
-
-    def _get_reward(self):
-        pass
 
     def _act(self, action):
         '''
@@ -43,22 +90,24 @@ class BitcoinTradeEnv():
         elif action == 1:
             if self.current_cash_value >= self.trading_open_price[self.trading_index+1]:
                 self.current_asset_unit += self.env_config.position_amount
-                self.current_cash_value -= self.trading_open_price[self.trading_index+1] * self.env_config.position_amount
+                self.current_cash_value -= self.trading_open_price[self.trading_index+1] * self.env_config.position_amount - self.env_config.trade_cost
+                if self.current_cash_value + self.current_asset_unit*self.trading_open_price[self.trading_index+1] >= self.total_capital_history[0]:
+                    reward += (self.current_cash_value + self.current_asset_unit*self.trading_open_price[self.trading_index+1] - self.total_capital_history[0]) / 10000
             else:
                 reward = -1
         elif action == 2:
-            if self.current_asset_unit * self.trading_open_price[self.trading_index+1] <= self.trading_open_price[self.trading_index+1]:
-                self.current_cash_value += self.trading_open_price[self.trading_index+1] * self.env_config.position_amount
+            if self.current_asset_unit >= self.env_config.position_amount:
+                self.current_cash_value += self.trading_open_price[self.trading_index+1] * self.env_config.position_amount - self.env_config.trade_cost
                 self.current_asset_unit -= self.env_config.position_amount
+                if self.current_cash_value + self.current_asset_unit*self.trading_open_price[self.trading_index+1] >= self.total_capital_history[0]:
+                    reward += (self.current_cash_value + self.current_asset_unit*self.trading_open_price[self.trading_index+1] - self.total_capital_history[0]) / 10000
             else:
                 reward = -1
         else:
             raise ValueError('action should only be picked from 0, 1, 2')
 
-        if self.current_cash_value + self.current_asset_unit*self.trading_open_price[self.trading_index+1] >= self.total_capital_history[-1]:
-            reward += 1
-
-        self.total_capital_history.append(self.current_asset_unit*self.trading_open_price[self.trading_index+1] + self.current_cash_value)
+        self.asset_cash_history.append([self.current_cash_value, self.current_asset_unit])
+        self.total_capital_history.append(self.current_cash_value + self.current_asset_unit*self.trading_open_price[self.trading_index+1])
         self.act_count += 1
         self.trading_index += 1
         if self.act_count == self.env_config.episode_length:
@@ -72,7 +121,10 @@ class BitcoinTradeEnv():
         :param action: action
         :return: next_obs, reward, done, extra_info
         '''
-        pass
+        reward, done = self._act(action)
+        self.obs = self._prep_obs()
+
+        return self.obs, reward, done, None
 
     def render(self):
         pass
@@ -84,9 +136,25 @@ if __name__ == '__main__':
     import os
     import yaml
     from collections import namedtuple
+    from itertools import count
 
     data_path = os.path.join('/'.join(os.getcwd().split('/')[:-2]), 'data/Binance_BTCUSDT_minute.csv')
     config_path = os.path.join('/'.join(os.getcwd().split('/')[:-1]), 'config/env_config.yml')
     env_config = yaml.load(open(config_path, 'r'))
     env_config = namedtuple('env_config', env_config.keys())(**env_config)
     bitcon_trade_env = BitcoinTradeEnv(data_path, env_config)
+    state = bitcon_trade_env.reset()
+    rewards = []
+    for _ in count():
+        action = np.random.randint(0, 3)
+        state, reward, done, _ = bitcon_trade_env.step(action)
+        rewards.append(reward)
+        print(action)
+        print(state)
+        print(reward)
+        print(done)
+        print(bitcon_trade_env.total_capital_history[-1])
+        print('-------------------------------------------------------------------------------')
+        if done:
+            break
+    print(np.mean(rewards))
