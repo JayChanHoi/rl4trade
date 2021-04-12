557a63d80ea9b83f7c82e02a89e0b5b7ef0b841a
Jay Chan <34962937+chan111222333@users.noreply.github.com>
2021-04-12 12:46:43 +0800

    update

--- src/rl/distributed/learner.py
+++ src/rl/distributed/learner.py
@@ -77,7 +77,7 @@ class LearnerR2D2(object):
                 action = action_value.argmax(dim=2).squeeze().item()
             else:
                 # sample action
-                action = torch.from_numpy(self.env.get_action_mask()).float().multinomial(1).item()
+                action = torch.from_numpy(self.eval_env.get_action_mask()).float().multinomial(1).item()
 
             obs, reward, done, _ = self.eval_env.step(action)
             reward_list.append(reward)
