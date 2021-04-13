d76f0247b422309d8ccb7457bb0d3a3bfbc13cf0
Jay Chan <34962937+chan111222333@users.noreply.github.com>
2021-04-13 12:57:26 +0800

    update

--- src/rl/distributed/learner.py
+++ src/rl/distributed/learner.py
@@ -7,6 +7,7 @@ import numpy as np
 from copy import deepcopy
 import os
 import random
+from itertools import count
 
 from ..utils import rescale, inv_rescale
 
@@ -89,97 +90,98 @@ class LearnerR2D2(object):
                 return [episode_reward, (self.eval_env.total_capital_history[-1] - self.eval_env.total_capital_history[0])/ 10000]
 
     def run(self):
-        memory_size = self.memory_server.memory_size.remote()
-        if ray.get(memory_size) >= self.learner_start_update_memory_size:
-            batch_memory, sample_indices, priority_prob = ray.get(self.memory_server.send_sample_to_learner.remote(
-                alpha=self.priority_alpha,
-                batch_size=self.batch_size
-            ))
-            # eg job_obs batch item has shape -> (b, sequence_length, hist_length, job_num, job_feature_dim)
-            batch_memory = [item.to(self.device) for item in batch_memory]
-            priority_prob = priority_prob.to(self.device)
-            priority_beta = self.priority_beta
+        for _ in count():
+            memory_size = self.memory_server.memory_size.remote()
+            if ray.get(memory_size) >= self.learner_start_update_memory_size:
+                batch_memory, sample_indices, priority_prob = ray.get(self.memory_server.send_sample_to_learner.remote(
+                    alpha=self.priority_alpha,
+                    batch_size=self.batch_size
+                ))
+                # eg job_obs batch item has shape -> (b, sequence_length, hist_length, job_num, job_feature_dim)
+                batch_memory = [item.to(self.device) for item in batch_memory]
+                priority_prob = priority_prob.to(self.device)
+                priority_beta = self.priority_beta
 
-            is_weight = ((priority_prob * self.memory_size_bound) ** priority_beta).reciprocal_()
-            normalized_is_weight = is_weight / is_weight.max()
+                is_weight = ((priority_prob * self.memory_size_bound) ** priority_beta).reciprocal_()
+                normalized_is_weight = is_weight / is_weight.max()
 
-            with torch.no_grad():
-                burn_in_state = batch_memory[2][:, :20, :, :]
-
-                _, core_net_burn_in_final_hidden_state = self.agent_core_net(
-                    burn_in_state,
-                    (batch_memory[4], batch_memory[5])
-                )
-
-            update_state = batch_memory[2][:, 20:-(self.nsteps+1), :, :]
-            action_value, final_hidden_state = self.agent_core_net(update_state, core_net_burn_in_final_hidden_state)
-
-            with torch.no_grad():
-                traget_net_action_value, _ = self.target_net(
-                    batch_memory[2][:, 20+self.nsteps+1:, :, :],
-                    (batch_memory[4], batch_memory[5])
-                )
-                action_value_extra, _ = self.agent_core_net(
-                    batch_memory[2][:, -(self.nsteps+1):, :, :],
-                    final_hidden_state
-                )
-
-                learner_action_value_for_max_action = torch.cat([action_value, action_value_extra], dim=1)[:, self.nsteps+1:, :]
-                learner_max_action = learner_action_value_for_max_action.argmax(dim=2, keepdim=True)
-
-            rewards_ = batch_memory[1][:, 20:-(self.nsteps+1)]
-            dones_ = batch_memory[-3][:, 20+(self.nsteps+1):]
-            # dones_ -> shape :(b. sequence_length)
-            non_terminal_mask = 1 - dones_.float()
-            terminal_mask = dones_.float()
-            action_value_target = rescale((rewards_ + (self.gamma ** (self.nsteps + 1))*(inv_rescale(traget_net_action_value.gather(2, learner_max_action)).squeeze())) * non_terminal_mask + rewards_ * terminal_mask)
-            td_error = (action_value_target - action_value.gather(2, batch_memory[0][:, 20:-(self.nsteps+1)].unsqueeze(-1)).squeeze())
-
-            with torch.no_grad():
-                priority = (0.9 * td_error.abs().max(dim=1)[0] + (1 - 0.9) * td_error.abs().mean(dim=1)).cpu()
-                self.memory_server.update_priority.remote(priority, sample_indices)
-                self.memory_server.trim_excessive_sample.remote()
-
-            loss = (normalized_is_weight * (td_error**2).mean(dim=1)).mean()
-            self.optimizer.zero_grad()
-            loss.backward()
-            self.optimizer.step()
-
-            self.write_log(self.writer, loss.item(), 'Loss', self.train_count)
-
-            if self.train_count > 0 and self.train_count % 10:
-                self.parameter_server.update_ps_state_dict.remote({k:v.cpu() for k, v in self.agent_core_net.state_dict().items()})
-
-            if self.train_count % self.target_net_update_frequency == 0 and self.train_count > 0:
-                self.target_net.load_state_dict({k: (1 - self.update_lambda)*v1 + self.update_lambda*v2 for k, v1, v2 in zip(self.agent_core_net.state_dict().keys(), self.target_net.state_dict().values(), self.agent_core_net.state_dict().values())})
-
-            if self.train_count % self.eval_frequency == 0 and self.train_count > 0:
                 with torch.no_grad():
-                    self.agent_core_net.eval()
-                    expected_reward, episodic_investment_return = self.eval(self.agent_core_net)
-                    self.writer.add_scalars(main_tag='expected reward', tag_scalar_dict={'learner':expected_reward}, global_step=self.train_count)
-                    self.writer.add_scalars(main_tag='episodic investment return ', tag_scalar_dict={'learner':episodic_investment_return}, global_step=self.train_count)
-                    self.writer.add_scalars(main_tag='memory size', tag_scalar_dict={'learner':ray.get(memory_size)}, global_step=iter)
+                    burn_in_state = batch_memory[2][:, :20, :, :]
 
-                    expected_reward, episodic_investment_return = self.eval(None)
-                    self.writer.add_scalars(main_tag='expected reward', tag_scalar_dict={'random agent':expected_reward}, global_step=self.train_count)
-                    self.writer.add_scalars(main_tag='episodic investment return ', tag_scalar_dict={'random agent':episodic_investment_return}, global_step=self.train_count)
-
-            self.agent_core_net.train()
-            if self.train_count % 5000 == 0 and self.train_count > 0:
-                checkpoint_dict = {'iter': iter + 1, 'state_dict': self.agent_core_net.state_dict()}
-                torch.save(
-                    checkpoint_dict,
-                    os.path.join(
-                        'checkpoint/{}'.format(self.model_name),
-                        '{}_checkpoint_episode_{}.pth'.format('distributed_dqn', self.train_count + 1)
+                    _, core_net_burn_in_final_hidden_state = self.agent_core_net(
+                        burn_in_state,
+                        (batch_memory[4], batch_memory[5])
                     )
-                )
 
-            self.train_count += 1
-            print('===============================train count : {}================================'.format(self.train_count))
-            print(loss.item())
+                update_state = batch_memory[2][:, 20:-(self.nsteps+1), :, :]
+                action_value, final_hidden_state = self.agent_core_net(update_state, core_net_burn_in_final_hidden_state)
 
-        else:
-            if iter % 10000 == 0:
-                self.writer.add_scalars(main_tag='memory size', tag_scalar_dict={'learner':ray.get(memory_size)}, global_step=iter)
+                with torch.no_grad():
+                    traget_net_action_value, _ = self.target_net(
+                        batch_memory[2][:, 20+self.nsteps+1:, :, :],
+                        (batch_memory[4], batch_memory[5])
+                    )
+                    action_value_extra, _ = self.agent_core_net(
+                        batch_memory[2][:, -(self.nsteps+1):, :, :],
+                        final_hidden_state
+                    )
+
+                    learner_action_value_for_max_action = torch.cat([action_value, action_value_extra], dim=1)[:, self.nsteps+1:, :]
+                    learner_max_action = learner_action_value_for_max_action.argmax(dim=2, keepdim=True)
+
+                rewards_ = batch_memory[1][:, 20:-(self.nsteps+1)]
+                dones_ = batch_memory[-3][:, 20+(self.nsteps+1):]
+                # dones_ -> shape :(b. sequence_length)
+                non_terminal_mask = 1 - dones_.float()
+                terminal_mask = dones_.float()
+                action_value_target = rescale((rewards_ + (self.gamma ** (self.nsteps + 1))*(inv_rescale(traget_net_action_value.gather(2, learner_max_action)).squeeze())) * non_terminal_mask + rewards_ * terminal_mask)
+                td_error = (action_value_target - action_value.gather(2, batch_memory[0][:, 20:-(self.nsteps+1)].unsqueeze(-1)).squeeze())
+
+                with torch.no_grad():
+                    priority = (0.9 * td_error.abs().max(dim=1)[0] + (1 - 0.9) * td_error.abs().mean(dim=1)).cpu()
+                    self.memory_server.update_priority.remote(priority, sample_indices)
+                    self.memory_server.trim_excessive_sample.remote()
+
+                loss = (normalized_is_weight * (td_error**2).mean(dim=1)).mean()
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+
+                self.write_log(self.writer, loss.item(), 'Loss', self.train_count)
+
+                if self.train_count > 0 and self.train_count % 10:
+                    self.parameter_server.update_ps_state_dict.remote({k:v.cpu() for k, v in self.agent_core_net.state_dict().items()})
+
+                if self.train_count % self.target_net_update_frequency == 0 and self.train_count > 0:
+                    self.target_net.load_state_dict({k: (1 - self.update_lambda)*v1 + self.update_lambda*v2 for k, v1, v2 in zip(self.agent_core_net.state_dict().keys(), self.target_net.state_dict().values(), self.agent_core_net.state_dict().values())})
+
+                if self.train_count % self.eval_frequency == 0 and self.train_count > 0:
+                    with torch.no_grad():
+                        self.agent_core_net.eval()
+                        expected_reward, episodic_investment_return = self.eval(self.agent_core_net)
+                        self.writer.add_scalars(main_tag='expected reward', tag_scalar_dict={'learner':expected_reward}, global_step=self.train_count)
+                        self.writer.add_scalars(main_tag='episodic investment return ', tag_scalar_dict={'learner':episodic_investment_return}, global_step=self.train_count)
+                        self.writer.add_scalars(main_tag='memory size', tag_scalar_dict={'learner':ray.get(memory_size)}, global_step=iter)
+
+                        expected_reward, episodic_investment_return = self.eval(None)
+                        self.writer.add_scalars(main_tag='expected reward', tag_scalar_dict={'random agent':expected_reward}, global_step=self.train_count)
+                        self.writer.add_scalars(main_tag='episodic investment return ', tag_scalar_dict={'random agent':episodic_investment_return}, global_step=self.train_count)
+
+                self.agent_core_net.train()
+                if self.train_count % 5000 == 0 and self.train_count > 0:
+                    checkpoint_dict = {'iter': iter + 1, 'state_dict': self.agent_core_net.state_dict()}
+                    torch.save(
+                        checkpoint_dict,
+                        os.path.join(
+                            'checkpoint/{}'.format(self.model_name),
+                            '{}_checkpoint_episode_{}.pth'.format('distributed_dqn', self.train_count + 1)
+                        )
+                    )
+
+                self.train_count += 1
+                print('===============================train count : {}================================'.format(self.train_count))
+                print(loss.item())
+
+            else:
+                if iter % 10000 == 0:
+                    self.writer.add_scalars(main_tag='memory size', tag_scalar_dict={'learner':ray.get(memory_size)}, global_step=iter)
